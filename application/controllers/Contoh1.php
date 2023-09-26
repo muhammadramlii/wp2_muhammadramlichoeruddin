bc348ea4dbbf37ead4da17e4e0101b5cf9efc6c4
muhammadramlichoeruddin <muhammadramli113@gmail.com>
2023-09-26 16:47:44 +0700

    menambahkan controler Contoh1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+  public function index()
+{
+echo "<h1>Perkenalkan</h1>";
+echo"Nama saya Muhammad Ramli
+saya tinggal di daerah duren sawit
+olah raga yang saya sukai bulutangkis";
+}
+}
+
